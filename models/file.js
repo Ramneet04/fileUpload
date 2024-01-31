67acb0a834113d0f05390316e286781e30a5ad1c
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-01-31 20:42:10 +0530

    addind mail functionality by nodemailer

--- models/file.js
+++ models/file.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
-
+require("dotenv").config();
 const fileSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -20,6 +20,20 @@ const fileSchema = new mongoose.Schema({
 fileSchema.post("save", async function(doc){
     try {
         console.log("DOC ->",doc);
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD
+            }
+        });
+        let info = await transporter.sendMail({
+            from:  "no-reply@imageuploader.com",
+            to:  doc.email,
+            subject: `Your Image has been uploaded`,
+            html: `<h2>Hello !!!</h2><p>We have Uploaded your file</p> <hr> <a href="${doc.imageUrl}">${doc.imageUrl}</a>`
+        });
+        console.log("Mail -> ",info);
     } catch (error) {
         console.log("NODEMAILER ERROR -> ", error);
     }
